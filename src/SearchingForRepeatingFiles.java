20f3b0053b2dd4307222e8a150b2382e3478ee2e
PrinterDoc <vlpure56@gmail.com>
2018-08-09 01:13:36 +0500

    09.08

--- src/SearchingForRepeatingFiles.java
+++ src/SearchingForRepeatingFiles.java
@@ -1,12 +1,12 @@
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
+import java.io.*;
+import java.lang.reflect.Array;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.*;
 import java.util.stream.Collectors;
+
+import static java.nio.charset.StandardCharsets.UTF_8;
 
 public class SearchingForRepeatingFiles {
 
@@ -18,13 +18,9 @@ public class SearchingForRepeatingFiles {
          *
          Return data: List of strings describing paths to files in folder
          */
-
-
         File directoryName = new File(pathName);
         int i = 0;
         List<String> returnList = new ArrayList<>();
-
-
         if (directoryName.isDirectory()) {
             if (directoryName.listFiles().length != 0) {
                 for (File item : directoryName.listFiles()) {
@@ -40,25 +36,6 @@ public class SearchingForRepeatingFiles {
     }
 
 
-    public static void compareDescFile(DescribePart descFile1, DescribePart descFile2){
-        if (descFile1.repeatCount == descFile2.repeatCount && descFile1.repeatPart == descFile2.repeatPart && descFile1.fileName != descFile2.fileName){
-            System.out.println("Файлы " + descFile1.fileName + " и " + descFile2.fileName +". " + " Повторяющаяся часть: " + descFile1.repeatPart + " Кол-во повторений: " + descFile1.repeatCount);
-        }
-    }
-
-
-    public static void printEqualFile(ArrayList<DescribePart> filesArray){
-
-        for(int i = 1; i<filesArray.size(); i++){
-            for(int j =i+1 ;j<filesArray.size();j++){
-                compareDescFile(filesArray.get(i), filesArray.get(j));
-            }
-        }
-    }
-
-
-
-
    // public static void getRepeatInfo(String pathName){
         /*
 
@@ -70,85 +47,110 @@ public class SearchingForRepeatingFiles {
                 .collect((Collectors.groupingBy(p -> p, Collectors.counting())))
                 .entrySet().stream().filter(t -> t.getValue() > 1)
                 .forEach(key -> System.out.println(key.getKey() + " " + key.getValue()));
-}
         */
 
 
 
+        public static HashMap<String,HashMap<String,Integer>> repeatCountMap = new HashMap<>();
+
+        public static void searchParts(String filePath){
+
+            try(FileInputStream fis=new FileInputStream(filePath))
+            {
+                char space = ' ';
+                int read = 0;
+                StringBuilder buffworld = new StringBuilder();
+                //HashMap<String,HashMap<String,Integer>> repeatCountMap = new HashMap<>();
+
+                while ( (read = fis.read())> 0){
 
 
+                    if ((char)read != space){
+                        buffworld.append((char)read);
+                    }
+
+
+                    if((char)read == space){
+                        String word = buffworld.toString();
+                        buffworld.delete(0,buffworld.length());
+                        
+
+                        if(!repeatCountMap.containsKey(word)){
+                        repeatCountMap.put(word, new HashMap<String,Integer>());
+                        repeatCountMap.get(word).put(filePath,1);
+                        }
+                         else {
+                            if (!repeatCountMap.get(word).containsKey(filePath)) {
+                                repeatCountMap.get(word).put(filePath, 1);
+                            } else {
+                                repeatCountMap.get(word).put(filePath, repeatCountMap.get(word).get(filePath) + 1);
+                            }
+                        }
+                    }
+                }
+            }
+            catch(IOException ex){
+
+
+            }
+
+
+
+        }
 
 
     public static void main(String[] args) {
 
-        List<String> filesPath =getFilePath("C:\\Users\\user\\Desktop\\SomeDir");
+        List<String> filesPath =getFilePath("C:\\Users\\PC\\Desktop\\SomeDir");
         //System.out.println(filesPath);
+        //FileInputStream fstream = new FileInputStream("C:\\Users\\PC\\Desktop\\SomeDir\\1.txt");
+       //System.out.println("увеличить");
+for(String fPath: filesPath) {
+    searchParts(fPath);
+}
+//searchParts("C:\\Users\\PC\\Desktop\\SomeDir\\2.txt");
 
+        //System.out.print(repeatCountMap);
 
-        //String[] testArr = {"lol","1","dSFd","df","dfg","fadf","gog","dGGD","lol","DGg","WGFdf","hfjk","gog","afbdv","gog","FDfef","gog","4r","dhjhgf","erg","ert","rt","uhgf","rgft"};
-         /*       String sa ;
-               Arrays.stream(testArr)
-                .collect((Collectors.groupingBy(p -> p, Collectors.counting())))
-                .entrySet().stream().filter(t -> t.getValue() > 1)
-                .forEach(key -> System.out.println(key.getKey() + " " + key.getValue()));
-*/
+        for(String repeatPart: repeatCountMap.keySet()){
+            HashMap<String, Integer> hashMap = repeatCountMap.get(repeatPart);
+            //for (String fileName: repeatCountMap.get(repeatPart).keySet()){
+                //int hashcode = repeatCountMap.get(repeatPart).get(fileName).hashCode();
+                //repeatCountMap.get(repeatPart).values()
+            HashMap<Integer, ArrayList<String>> result = new LinkedHashMap<Integer, ArrayList<String>>();
 
-            DescribePart desc1 = new DescribePart("lol.txt",5,"dfdf.txt");
-          DescribePart desc3 = new DescribePart("lol.txt",5,"dff.txt");
-          DescribePart desc4 = new DescribePart("lol.txt",5,"dfdf.txt");
-          DescribePart desc7 = new DescribePart("loltx",2,"df5.txt");
-         DescribePart desc8 = new DescribePart("loltx",2,"df.txt");
-
-        ArrayList<DescribePart> fileArray = new ArrayList<>();
-        fileArray.add(desc8);
-        fileArray.add(desc7);
-        fileArray.add(desc3);
-        fileArray.add(desc4);
-        fileArray.add(desc1);
-//
-       // printEqualFile(fileArray);
-        /*int[] array = {11, 14, 17, 11, 48, 33, 29, 11, 17, 22, 11, 48, 18};
-
-        Map<Integer, Long> occurrences = Arrays.stream(array)
-                .boxed()
-                .collect(Collectors.groupingBy(
-                        Function.identity(),
-                        Collectors.counting()));
-                System.out.println(occurrences);
-              */
-
-
-
-
-
-
-        final int BUFFER_SIZE = 512;
-        try {
-            FileInputStream fis = new FileInputStream("C:\\Users\\user\\Desktop\\SomeDir\\1.txt");
-            byte[] bufferPart = new byte[BUFFER_SIZE];
-            Map<String,Long> buffMap = new HashMap<>();
-            int read = 0;
-            while ( (read = fis.read(bufferPart))> 0) {
-              String buffStr = new String(bufferPart,"UTF-8");
-              String[] buffArray = buffStr.split(" ");
-                        Arrays.stream(buffArray)
-                        .collect((Collectors.groupingBy(p -> p, Collectors.counting())))
-                        .entrySet().stream().filter(t -> t.getValue() > 1)
-                        .forEach(key -> buffMap.put(key.getKey(),key.getValue())/*key -> System.out.println(key.getKey() + " " + key.getValue())*/);
-
-
-                Arrays.stream(buffArray)
-                        .collect((Collectors.groupingBy(p -> p, Collectors.counting())))
-                        .entrySet().stream().filter(t -> t.getValue() > 1)
-                        .forEach(/*key -> buffMap.put(key.getKey(),key.getValue())*/key -> System.out.println(key.getKey() + " " + key.getValue()));
+            for (String key : hashMap.keySet()) {
+                ArrayList<String> colName = null;
+                if (!result.containsKey(hashMap.get(key))) {
+                    colName = new ArrayList<String>();
+                    colName.add(key);
+                    result.put(hashMap.get(key), colName);
+                } else {
+                    colName = result.get(hashMap.get(key));
+                    colName.add(key);
+                    result.put(hashMap.get(key), colName);
                 }
-            System.out.println(buffMap);
-            fis.close();
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
+
+
+            }
+
+            for (Integer key : result.keySet()) {
+                if(key>2 && result.get(key).size() >1 )
+                    {
+                        System.out.print("Повторяющаяся часть: "+repeatPart+" Количество повторений: "+ key + " Файлы: " + result.get(key) + "\n");
+                    }
+                }
+
+
+
+
+
         }
+
+
+
+
+
 
 
     }
